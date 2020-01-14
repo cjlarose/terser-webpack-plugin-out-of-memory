66c80e5797698c60cb6fe29d0dc48203ef507b2a
Chris LaRose <cjlarose@gmail.com>
2020-01-13 17:01:28 -0800

    Update script to yield password strength

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello');
+const zxcvbn = require('zxcvbn');
+
+console.log('Enter a password to calculate its strength');
+
+process.stdin.on('data', function (data) {
+  const password = data.toString('utf8').trim();
+  const result = zxcvbn(password);
+  console.log(result);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -13,4 +13,5 @@ module.exports = {
       new TerserPlugin(),
     ],
   },
+  target: 'node',
 };
